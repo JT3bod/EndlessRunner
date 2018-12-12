818c1137d93e5d91473fdac4bec9f965815706bb
JT3bod <james3bod@gmail.com>
2018-12-12 14:42:04 +0000

    StartScreenAdded

---
+++ Assets/StartingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartingScript : MonoBehaviour {
+    public Button start;
+    public Button quit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StartButton_Clicked()
+    {
+        SceneManager.LoadScene("Level1");
+
+    }
+    public void QuitButton_Clicked()
+    {
+        Application.Quit();
+    }
+}
