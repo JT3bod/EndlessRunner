4a74466d95710235d7977a8282864cc134c297b1
JT3bod <james3bod@gmail.com>
2018-12-12 19:06:45 +0000

    background
    works except character seems to collide.

---
+++ Assets/HouseScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseScript : MonoBehaviour {
+    public Animator anim;
+    public Slider sliderInstance;
+
+    bool broken = false;
+    // Use this for initialization
+    void Start () {
+        anim.Play("house");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (broken)
+        {
+            anim.Play("houseBroken");
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            if (sliderInstance.value == 100)
+            {
+                anim.Play("house broken");
+                broken = true;
+            }
+        }
+	}
+}
